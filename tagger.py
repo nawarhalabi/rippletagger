afd4f8e2715f9d05ba04f441449b2c5ed8d38a03
Emil Stenström <em@kth.se>
2016-08-12 16:11:30 +0200

    Simplify method names.

--- tagger.py
+++ tagger.py
@@ -5,12 +5,12 @@ from learner.tree import SCRDRTree
 from learner.fwobject import FWObject
 from learner.utils import readDictionary
 
-class RDRPOSTagger(SCRDRTree):
+class Tagger(SCRDRTree):
     def __init__(self, model_path):
         self.constructSCRDRtreeFromRDRfile(model_path + ".RDR")
         self.DICT = readDictionary(model_path + ".DICT")
 
-    def tagRawSentence(self, line):
+    def tag(self, line):
         guessed_tags = initializeSentence(self.DICT, line.encode("utf-8"))
         sentence = []
 
@@ -23,8 +23,8 @@ class RDRPOSTagger(SCRDRTree):
 
         return sentence
 
-r = RDRPOSTagger("Models/UniPOS/UD_French/train.UniPOS")
-print r.tagRawSentence(u"Cette annonce a fait l' effet d' une véritable bombe .")
+r = Tagger("Models/UniPOS/UD_French/train.UniPOS")
+print r.tag(u"Cette annonce a fait l' effet d' une véritable bombe .")
 
-r = RDRPOSTagger("Models/UniPOS/UD_Swedish/train.UniPOS")
-print r.tagRawSentence(u"Fördomen har alltid sin rot i vardagslivet")
+r = Tagger("Models/UniPOS/UD_Swedish/train.UniPOS")
+print r.tag(u"Fördomen har alltid sin rot i vardagslivet")
