83c1b9cbf9d05f414eb9b9658fe3901c997a2bd5
Emil Stenström <em@kth.se>
2016-08-12 18:51:05 +0200

    Refactor initial_tagger into a real tagger.

--- rippletagger/tagger.py
+++ rippletagger/tagger.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 
-from rippletagger.tagger_initial import initializeSentence
+import re
+
 from rippletagger.models import SCRDRTree, FeatureVector
 from rippletagger.utils import readDictionary
 
@@ -14,17 +15,68 @@ class Tagger(SCRDRTree):
         self.DICT = readDictionary(model_path + ".DICT")
 
     def tag(self, line):
-        guessed_tags = initializeSentence(self.DICT, line)
+        tagger = FrequencyTagger(self.DICT)
+        guessed_tags = tagger.tag(line)
         sentence = []
 
         for i, guessed_tag in enumerate(guessed_tags):
             feature = FeatureVector.getFeatureVector(guessed_tags, i)
-            word, tag = guessed_tag
             node = self.findFiredNode(feature)
+            word, tag = guessed_tag
 
             sentence.append((word, node.conclusion if node.depth > 0 else tag))
 
         return sentence
+
+class FrequencyTagger:
+    def __init__(self, FREQDICT):
+        self.FREQDICT = FREQDICT
+
+    def tag(self, line):
+        words = line.strip().split()
+        taggedSen = []
+        for word in words:
+            if word in [u"“", u"”", u"\""]:
+                taggedSen.append("''/" + self.FREQDICT["''"])
+                continue
+
+            tag = ''
+            decodedW = word
+            lowerW = decodedW.lower()
+            if word in self.FREQDICT:
+                tag = self.FREQDICT[word]
+            elif lowerW in self.FREQDICT:
+                tag = self.FREQDICT[lowerW]
+            else:
+                if re.search(r"[0-9]+", word) is not None:
+                    tag = self.FREQDICT["TAG4UNKN-NUM"]
+                else:
+                    suffixL2 = suffixL3 = suffixL4 = suffixL5 = None
+                    wLength = len(decodedW)
+                    if wLength >= 4:
+                        suffixL3 = ".*" + decodedW[-3:]
+                        suffixL2 = ".*" + decodedW[-2:]
+                    if wLength >= 5:
+                        suffixL4 = ".*" + decodedW[-4:]
+                    if wLength >= 6:
+                        suffixL5 = ".*" + decodedW[-5:]
+
+                    if suffixL5 in self.FREQDICT:
+                        tag = self.FREQDICT[suffixL5]
+                    elif suffixL4 in self.FREQDICT:
+                        tag = self.FREQDICT[suffixL4]
+                    elif suffixL3 in self.FREQDICT:
+                        tag = self.FREQDICT[suffixL3]
+                    elif suffixL2 in self.FREQDICT:
+                        tag = self.FREQDICT[suffixL2]
+                    elif decodedW[0].isupper():
+                        tag = self.FREQDICT["TAG4UNKN-CAPITAL"]
+                    else:
+                        tag = self.FREQDICT["TAG4UNKN-WORD"]
+
+            taggedSen.append((word, tag))
+
+        return taggedSen
 
 class LanguageMapper:
     def __init__(self):
