1bb0ae28dbb1d9ec4572b577a785a6f698b70ecb
Emil Stenström <em@kth.se>
2016-08-12 17:22:15 +0200

    Allow API calls with just a language code or name.

--- rippletagger/tagger.py
+++ rippletagger/tagger.py
@@ -6,8 +6,11 @@ from rippletagger.fwobject import FWObject
 from rippletagger.utils import readDictionary
 
 class Tagger(SCRDRTree):
-    def __init__(self, langauge):
-        model_path = "Models/UD_%s/train.UniPOS" % langauge
+    def __init__(self, code_or_name):
+        mapper = LanguageMapper()
+        directory_name = mapper.directory_name(code_or_name)
+        model_path = "Models/UD_%s/train.UniPOS" % directory_name
+
         self.constructSCRDRtreeFromRDRfile(model_path + ".RDR")
         self.DICT = readDictionary(model_path + ".DICT")
 
@@ -23,3 +26,38 @@ class Tagger(SCRDRTree):
             sentence.append((word, node.conclusion if node.depth > 0 else tag))
 
         return sentence
+
+class LanguageMapper:
+    def __init__(self):
+        with open("models/language_mapping.txt", "r") as f:
+            mapping_lines = [line.strip() for line in f.readlines()]
+
+        language_twocode = {}
+        language_threecode = {}
+        language_name = {}
+
+        for line in mapping_lines:
+            if line.startswith("#") or not line.strip():
+                continue
+
+            twocode, threecode, name, directory_name = line.split(", ")
+            language_twocode[twocode] = directory_name.strip()
+            language_threecode[threecode] = directory_name.strip()
+            language_name[name] = directory_name
+
+        self.language_twocode = language_twocode
+        self.language_threecode = language_threecode
+        self.language_name = language_name
+
+    def directory_name(self, code_or_name):
+        if code_or_name in self.language_twocode:
+            return self.language_twocode[code_or_name]
+        elif code_or_name in self.language_threecode:
+            return self.language_threecode[code_or_name]
+        elif code_or_name in self.language_name:
+            return self.language_name[code_or_name]
+
+        raise Exception(
+            "Language '%s' not found. See models/language_mapping.txt"
+            " for valid language codes" % code_or_name
+        )
--- test.py
+++ test.py
@@ -2,8 +2,8 @@
 
 from rippletagger.tagger import Tagger
 
-r = Tagger("French")
+r = Tagger("fr")
 print r.tag(u"Cette annonce a fait l' effet d' une véritable bombe .")
 
-r = Tagger("Swedish")
+r = Tagger("sv")
 print r.tag(u"Fördomen har alltid sin rot i vardagslivet")
