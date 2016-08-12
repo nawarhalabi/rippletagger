6076fb9c92e90b3b6969b143978d8a8e1482e59b
Emil Stenström <em@kth.se>
2016-08-12 19:31:12 +0200

    Make test.py a test.

--- test.py
+++ test.py
@@ -3,7 +3,33 @@
 from rippletagger.tagger import Tagger
 
 r = Tagger(language="fr")
-print r.tag(u"Cette annonce a fait l' effet d' une véritable bombe .")
+assert (
+    r.tag(u"Cette annonce a fait l' effet d' une véritable bombe .") ==
+    [
+        (u'Cette', 'DET'),
+        (u'annonce', 'NOUN'),
+        (u'a', 'AUX'),
+        (u'fait', 'VERB'),
+        (u"l'", 'DET'),
+        (u'effet', 'NOUN'),
+        (u"d'", 'ADP'),
+        (u'une', 'DET'),
+        (u'véritable', 'ADJ'),
+        (u'bombe', 'NOUN'),
+        (u'.', 'PUNCT')
+    ]
+)
 
 r = Tagger(language="sv")
-print r.tag(u"Fördomen har alltid sin rot i vardagslivet")
+assert (
+    r.tag(u"Fördomen har alltid sin rot i vardagslivet") ==
+    [
+        (u'Fördomen', 'NOUN'),
+        (u'har', 'AUX'),
+        (u'alltid', 'ADV'),
+        (u'sin', 'PRON'),
+        (u'rot', 'NOUN'),
+        (u'i', 'ADP'),
+        (u'vardagslivet', 'NOUN')
+    ]
+)
